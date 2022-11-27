44534d1bcad0be0e0f819fe146542a04faf6144a
Syed Zawad <syedzawadd@gmail.com>
2022-11-27 11:18:46 +0600

    jwt api created

--- index.js
+++ index.js
@@ -49,6 +49,17 @@ async function run() {
         })
 
 
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            console.log(user);
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' });
+                return res.send({ accessToken: token });
+            }
+            res.status(403).send({ accessToken: '' })
+        })
         app.post('/users', async (req, res) => {
             const user = req.body;
             const result = await userCollection.insertOne(user);
